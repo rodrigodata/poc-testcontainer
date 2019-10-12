1b2ca7fcf2c965f16bd864bf1e6fd05ec5afb8d2
Rodrigo Andrade <warodrigoandrade@gmail.com>
2019-10-12 16:31:11 -0300

    Finalizado setup e configuracao de primeiro cenario utilizando testcontainer.

--- src/test/kotlin/me/data/poctestcontainer/HealthChecktest.kt
+++ src/test/kotlin/me/data/poctestcontainer/HealthChecktest.kt
@@ -1,39 +1,54 @@
 package  me.data.poctestcontainer
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
 import org.testcontainers.containers.PostgreSQLContainer
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.output.Slf4jLogConsumer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.images.builder.ImageFromDockerfile
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-// import org.junit.jupiter.api.Test
-
-import java.sql.DriverManager
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.file.Paths
+import java.util.stream.Collectors
 
 @Testcontainers
 class HealthChecktest {
 
+    private var log = LoggerFactory.getLogger(HealthChecktest::class.java)
+
     @Container
-    private val container = PostgreSQLContainer<Nothing>("postgres:11")
-
-
-    @Test
-    fun `should perform simple query`() {
-
-        container.start()
-
-        DriverManager.getConnection(container.jdbcUrl, container.username, container.password).use { conn ->
-
-            conn.createStatement().use { stmt ->
-
-                stmt.executeQuery("SELECT 1").use { resultSet ->
-
-                    resultSet.next()
-
-                    assertThat(resultSet.getInt(1)).isEqualTo(1)
-                }
-            }
-        }
+    private val container: GenericContainer<Nothing> = GenericContainer<Nothing>(
+            ImageFromDockerfile()
+                    .withFileFromPath("./target/poctestcontainer-2.1.9.RELEASE.jar", Paths.get("./target/poctestcontainer-2.1.9.RELEASE.jar"))
+                    .withFileFromPath("Dockerfile", Paths.get("./Dockerfile"))
+    ).apply {
+        withExposedPorts(8080)
+        waitingFor(Wait.forLogMessage(".*Started PoctestcontainerApplicationKt.*", 1))
+        start()
+        followOutput(Slf4jLogConsumer(log))
     }
 
+    @Test
+    fun `should perform an api request healthcheck`() {
+
+        Assertions.assertThat(container.isRunning).isTrue()
+
+        val url = URL("http://${container.containerIpAddress}:${container.firstMappedPort}/healthcheck")
+
+        val conn = url.openConnection() as HttpURLConnection
+
+        Assertions.assertThat(conn.responseCode).isEqualTo(200)
+
+        File("response_body.txt").writeText(BufferedReader(InputStreamReader(conn.getInputStream())).lines().collect(Collectors.joining()))
+
+    }
 
 }
